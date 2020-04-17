d5e6fb5efa9c0b15478845497bc8a2d9ce387ca8
tefiavalos <emlavalos92@gmail.com>
2020-04-16 22:04:44 -0300

    trabajamos en clase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Charmander from './components/Charmander';
+import Squirtle from './components/Squirtle';
+import Bulbasaur from './components/Bulbasaur';
+import Pikachu from './components/Pikachu';
 import './App.css';
 
+
 function App() {
+  const [page, setPage] = useState("pikachu")
+  const [pokemon, setPokemon] = useState({})
+  const handleClick = e => {
+    setPage(e.target.id)
+  }
+  const paginacion = {
+    charmander: <Charmander  nombre={pokemon.name} peso={pokemon.weight} altura={pokemon.height} img={pokemon.sprites && pokemon.sprites.front_default}/>,
+    squirtle: <Squirtle nombre={pokemon.name} peso={pokemon.weight} altura={pokemon.height}img={pokemon.sprites && pokemon.sprites.front_default}/>,
+    bulbasaur: <Bulbasaur nombre={pokemon.name} peso={pokemon.weight} altura={pokemon.height}img={pokemon.sprites && pokemon.sprites.front_default}/>,
+    pikachu: <Pikachu nombre={pokemon.name} peso={pokemon.weight} altura={pokemon.height}img={pokemon.sprites && pokemon.sprites.front_default}/>,
+
+  }
+  useEffect(()=> {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${page}`)
+    .then(res => res.json())
+    .then(data => setPokemon(data))
+  }, [page])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemones</h1>
+      <section>
+        <button id="charmander" onClick={handleClick}>Charmander</button>
+        <button id="squirtle" onClick={handleClick}>Squirtle</button>
+        <button id="bulbasaur" onClick={handleClick}>Bulbasaur</button>
+        <button id="pikachu" onClick={handleClick}>Pikachu</button>
+      </section>
+      {paginacion[page]}
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Bulbasaur.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Bulbasaur = ({nombre, peso, altura, img}) => {
+    return (
+    <div>
+        <h2>{nombre}</h2>
+        <img src={img} alt={nombre}></img>
+        <p>El pokemon se llama {nombre}, mide {altura} cm y pesa {peso} kg.  </p>
+    </div>
+    )
+}
+export default Bulbasaur;
